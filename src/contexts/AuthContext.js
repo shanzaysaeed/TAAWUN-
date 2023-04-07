a51aa6391a5f97fcc3024051c67ddcf3657853ca
Muhammad Shahmeer <24100237@lums.edu.pk>
2023-04-08 01:14:43 +0500

    Issue Fixed: Auth

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -11,15 +11,16 @@ import {  collection, query, where, getDocs  } from "firebase/firestore";
 const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState({ role: '' });
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : { role: "" };
+  });
 
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const signIn = async (email, password) => {
-    console.log(email)
-    // const { user } = await signInWithEmailAndPassword(auth, email, password);
     await signInWithEmailAndPassword(auth, email, password);
     
     const ngoRef = collection(db, 'users');
@@ -28,25 +29,43 @@ export const AuthContextProvider = ({ children }) => {
     if (!querySnapshot.empty) {
       const doc = querySnapshot.docs[0];
       const userData = doc.data();
-      console.log(userData.role);
       setUser({ ...userData, role: userData.role });
+      localStorage.setItem("user", JSON.stringify({ ...userData, role: userData.role }));
     }
   };
   
-
   const logout = () => {
+    localStorage.removeItem("user");
     return signOut(auth);
   };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
       console.log(currentUser);
-      setUser(currentUser);
+      if (currentUser) {
+        const ngoRef = collection(db, 'users');
+        const q = query(ngoRef, where('email', '==', currentUser.email));
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+          const doc = querySnapshot.docs[0];
+          const userData = doc.data();
+          localStorage.setItem('user', JSON.stringify({ ...userData, role: userData.role }));
+          setUser({ ...userData, role: userData.role });
+        }
+      } else {
+        localStorage.removeItem('user');
+        setUser(null);
+      }
     });
+    const localUser = JSON.parse(localStorage.getItem('user'));
+    if (localUser) {
+      setUser(localUser);
+    }
     return () => {
       unsubscribe();
     };
   }, []);
+  
 
   return (
     <UserContext.Provider value={{ createUser, user, logout, signIn }}>
--- src/contexts/ProtectedRoute.js
+++ src/contexts/ProtectedRoute.js
@@ -4,18 +4,21 @@ import { UserAuth } from './AuthContext';
 
 const RoleProtectedRoute = ({ role, children }) => {
   const { user } = UserAuth();
+  const localUser = JSON.parse(localStorage.getItem('user'));
 
   console.log("database: ", user.role, " my: ", role)
+  console.log("p: ", localStorage.getItem("user"))
   
-  if (!user) {
+  if (!user && !localUser) {
     return <Navigate to='/' />;
   }
 
-  if (user.role !== role) {
+  if ((user && user.role !== role) || (localUser && localUser.role !== role)) {
     return <Navigate to='/' />;
   }
 
   return children;
 };
+
 
 export default RoleProtectedRoute;

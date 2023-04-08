7cc6cfaccef0ab5786401efe6008306955a6c860
maryam1320 <118680731+maryam1320@users.noreply.github.com>
2023-04-09 03:45:23 +0500

    NGO collab 2 screens frontend

--- Maryam/ngo-collab-frontend/src/collab1.js
+++ Maryam/ngo-collab-frontend/src/collab1.js
@@ -83,48 +83,40 @@ function App() {
                 </div>
 
                 <div className='flex flex-row w-full justify-between'>
-                    <div className='flex flex-col mt-6 mx-14'>
+                    <div className='flex flex-col mt-2 mx-14'>
                         <div className='tracking-tight text-black font-sans text-2xl font-semibold'>
-                            <p>Looking to collaborate with an NGO?</p>
+                            <p>Please select an NGO you want to collaborate with:</p>
                         </div>
-                    </div>
-
-                    <div className='p-3 mr-20 flex items-start justify-center items-center '>
-                        <button className='bg-[#26235C] w-40 h-10 text-white text-lg font-semibold rounded-lg border border-black flex justify-center items-center hover:bg-gray-500 '>
-                            Collaborate
-                            <span className='text-4xl mb-2 ml-3'>+</span>
-
-                        </button>
                     </div>
 
                 </div>
 
-                <div className=" my-6 mx-10 w-11/12 h-2/3 rounded">
+                <div className="mb-16 mx-10 w-11/12 h-2/3 rounded">
 
                     <div className='flex flex-row w-3/4 h-40 mt-3 ml-32 justify-center'>
                         <div className='flex flex-col border border-black shadow-md w-40 bg-white rounded-2xl h-40 justify-center mr-28'>
-                            <div className='flex flex-col  mx-4  mb-8 justify-center items-center'>
+                            <button className='flex flex-col  mx-4  mb-8 justify-center items-center'>
                                 <img src={akh} className=' w-12 h-12 mt-8'></img>
                                 <p className='font-bold text-lg '>Akhuwat</p>
                                 <p className='text-sm text-black whitespace-nowrap mb-2'>Flood Relief in Sindh </p>
-                                <button className='bg-[#26235C] text-white rounded-md  w-full h-8 '>Collaborate!</button>
-                            </div>
+                                <button className='bg-[#BFBFBF] text-black rounded-md hover:bg-green-500 w-full h-8 '>Select</button>
+                            </button>
                         </div>
                         <div className='flex flex-col border border-black w-40 bg-white rounded-2xl h-40 justify-center mr-28'>
-                            <div className='flex flex-col  mx-4  mb-8 justify-center items-center'>
+                            <button className='flex flex-col  mx-4  mb-8 justify-center items-center'>
                                 <img src={jdc} className=' w-12 h-12 mt-8'></img>
                                 <p className='font-bold text-lg '>JDC Pakistan</p>
                                 <p className='text-sm text-black whitespace-nowrap mb-2'>Fire Relief </p>
-                                <button className='bg-[#26235C] text-white rounded-md  w-full h-8 '>Collaborate!</button>
-                            </div>
+                                <button className='bg-[#BFBFBF] text-black rounded-md hover:bg-green-500 w-full h-8 '>Select</button>
+                            </button>
                         </div>
                         <div className='flex flex-col border border-black w-40 bg-white rounded-2xl h-40 justify-center'>
-                            <div className='flex flex-col  mx-4  mb-8 justify-center items-center'>
+                            <button className='flex flex-col  mx-4  mb-8 justify-center items-center'>
                                 <img src={edhi} className=' w-14 h-14 mt-6'></img>
                                 <p className='font-bold text-lg '>Edhi</p>
                                 <p className='text-sm text-black whitespace-nowrap mb-2'>Education in Sindh </p>
-                                <button className='bg-[#26235C] text-white rounded-md  w-full h-8 '>Collaborate!</button>
-                            </div>
+                                <button className='bg-[#BFBFBF] text-black rounded-md hover:bg-green-500 w-full h-8 '>Select</button>
+                            </button>
                         </div>
                     </div>
                     
@@ -138,34 +130,39 @@ function App() {
                     </div>
 
 
-                    <div className='flex flex-row ml-32 w-3/4 h-40 justify-center mb-6'>
+                    <div className='flex flex-row ml-32 w-3/4 h-40 justify-center mb-2'>
                         <div className='flex flex-col border border-black shadow-md w-40 bg-white rounded-2xl h-40 justify-center mr-28'>
-                            <div className='flex flex-col  mx-4  mb-8 justify-center items-center'>
+                            <button className='flex flex-col  mx-4  mb-8 justify-center items-center'>
                                 <img src={chiipa} className=' w-12 h-12 mt-8'></img>
                                 <p className='font-bold text-lg'>Chiipa</p>
                                 <p className='text-sm text-black whitespace-nowrap mb-2'>Food Drive </p>
-                                <button className='bg-[#26235C] text-white rounded-md  w-full h-8'>Collaborate!</button>
-                            </div>
+                                <button className='bg-[#BFBFBF] text-black rounded-md hover:bg-green-500 w-full h-8'>Select</button>
+                            </button>
                         </div>
                         <div className='flex flex-col border border-black w-40 bg-white rounded-2xl h-40 justify-center mr-28'>
-                            <div className='flex flex-col  mx-4  mb-8 justify-center items-center'>
+                            <button className='flex flex-col  mx-4  mb-8 justify-center items-center'>
                                 <img src={jdc} className=' w-12 h-12 mt-8'></img>
                                 <p className='font-bold text-lg'>JDC Pakistan</p>
                                 <p className='text-sm text-black whitespace-nowrap mb-2'>Flood Relief </p>
-                                <button className='bg-[#26235C] text-white rounded-md  w-full h-8'>Collaborate!</button>
-                            </div>
+                                <button className='bg-[#BFBFBF] text-black rounded-md hover:bg-green-500  w-full h-8'>Select</button>
+                            </button>
                         </div>
                         <div className='flex flex-col border border-black w-40 bg-white rounded-2xl h-40 justify-center'>
-                            <div className='flex flex-col  mx-4  mb-8 justify-center items-center'>
+                            <button className='flex flex-col  mx-4  mb-8 justify-center items-center'>
                                 <img src={khid} className=' w-12 h-12 mt-8'></img>
                                 <p className='font-bold text-lg '>Al-khidmat</p>
                                 <p className='text-sm text-black whitespace-nowrap mb-2'>Old-age homes</p>
-                                <button className='bg-[#26235C] text-white rounded-md  w-full h-8'>Collaborate!</button>
-                            </div>
+                                <button className='bg-[#BFBFBF] text-black hover:bg-green-500 rounded-md  w-full h-8'>Select</button>
+                            </button>
 
                         </div>
 
+                    </div>
 
+                    <div className='p-3 flex items-start justify-center items-center mt-4 '>
+                        <button className='bg-[#26235C] w-40 h-10 text-white text-lg font-semibold rounded-lg border border-black flex justify-center items-center hover:bg-gray-500 '>
+                            Collaborate
+                        </button>
                     </div>
 
                 </div>
